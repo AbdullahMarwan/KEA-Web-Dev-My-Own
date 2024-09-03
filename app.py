a07b9a5e347cb1d10100e0065aa89ab758d554cb
AbdullahMarwan <abdu.moussa01@gmail.com>
2024-09-03 09:49:08 +0200

    WIP adding CRUD operations

--- app.py
+++ app.py
@@ -15,6 +15,32 @@ def _():
 def _():
     return static_file("app.js", ".")
 
+# API
+####################################
+"""
+CREATE READ UPDATE DELETE (Known as CRUD)
+
+HTTP METHOD GET (To read data)
+/items
+
+HTTP METHOD POST (To create data)
+/items
+
+HTTP METHOD PUT (To update data)
+/items/<id>
+
+HTTP METHOD DELETE (To delete data)
+/items/<id>
+
+"""
+
+####################################
+@get("/items")
+def _():
+    return "items"
+
+
+
 ####################################
 run(host="0.0.0.0", port=80, debug=True, reloader=True, interval=0.3)
 
